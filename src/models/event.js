1ad7e3cc5c37cf1702c537d8e9466660e9e09a04
sara <aleja@gmail.com>
2026-04-27 14:04:02 -0500

    Creación constructor event.js

---
+++ src/models/event.js
@@ -0,0 +1,23 @@
+const eventSchema = new mongoose.Schema({
+  titulo: {
+    type: String,
+    required: true
+  },
+  fecha: {
+    type: Date,
+    required: true
+  },
+  ubicacion: String,
+  asistentes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }],
+  probabilidadInasistencia: {
+    type: Number,
+    default: 0
+  },
+  comunidad: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Community"
+  }
+}, { timestamps: true });
