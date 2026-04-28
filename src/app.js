85799d699ed8711bb79bfb082fc26f7fcd87d762
d <d@gmail.com>
2026-04-27 20:25:51 -0500

    Se realizó la conexión en app.js de las rutas de comunidad al servidor

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 
 // CAMBIO AQUÍ: Debe tener el ".routes" para que coincida con tu archivo
 const authRoutes = require('./routes/auth.routes'); 
+const communityRoutes = require('./routes/community.routes'); // <- AGREGAR
 
 const app = express();
 
@@ -18,6 +19,7 @@ app.use(express.json());
 
 // Rutas
 app.use('/api/auth', authRoutes); // 2. Conectar las rutas con un prefijo
+app.use('/api/community', communityRoutes); // <- AGREGAR
 
 app.get('/', (req, res) => res.send('API de Nexo funcionando 🚀'));
 
