633861f7c77eb498055822f4d0224afc97a20b9a
Brayan <brayans.rodriguezc@konradlorenz.edu.co>
2026-04-27 22:59:51 -0500

    Modelado completo de event.routes.js

--- src/routes/event.routes.js
+++ src/routes/event.routes.js
@@ -3,4 +3,9 @@ const router = express.Router();
 const eventController = require('../controllers/eventController');
 const protect = require('../middleware/authMiddleware');
 
+router.post('/', protect, eventController.crearEvento);
+router.get('/', protect, eventController.obtenerEventos);
+router.put('/:id', protect, eventController.actualizarEvento);
+router.delete('/:id', protect, eventController.eliminarEvento);
+
 module.exports = router;
