c2c6c80c7a971080a7b6d56bc321a770fd4fa297
Joseph Mendieta <jyma1204@gmail.com>
2026-04-28 17:08:10 -0500

    Corrección de community.routes.js

--- src/routes/community.routes.js
+++ src/routes/community.routes.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const communityController = require('../controllers/communityController');
 
 // Crear comunidad
-router.post('/community', communityController.crearComunidad);
+router.post('/', communityController.crearComunidad);
 
 // Listar comunidades
-router.get('/community', communityController.obtenerComunidades);
+router.get('/', communityController.obtenerComunidades);
 
 // Editar comunidad
-router.put('/community/:id', communityController.actualizarComunidad);
+router.put('/:id', communityController.actualizarComunidad);
 
 // Eliminar comunidad
-router.delete('/community/:id', communityController.eliminarComunidad);
+router.delete('/:id', communityController.eliminarComunidad);
 
 module.exports = router;
